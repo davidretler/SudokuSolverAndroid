b61352f40cf462674fe74ac5a3f60f6fc43d4772
davidretler <davidretler@gmail.com>
2015-12-21 10:43:35 -0500

    Finishing up UI

--- app/src/main/java/com/davidretler/sudokusolver/SudokuCell.java
+++ app/src/main/java/com/davidretler/sudokusolver/SudokuCell.java
@@ -1,17 +1,21 @@
 package com.davidretler.sudokusolver;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
 import android.content.res.TypedArray;
+import android.graphics.Color;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by david on 12/18/15.
  */
-public class SudokuCell extends EditText {
+public class SudokuCell extends TextView {
 
     public SudokuCell(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -26,78 +30,43 @@ public class SudokuCell extends EditText {
         a.recycle();
 
         this.addTextChangedListener(new CellListener());
+        this.setOnFocusChangeListener(new FocusListener());
     }
 
     private class CellListener implements TextWatcher {
 
-        String oldText;
-        String newText;
-
-        /**
-         * This method is called to notify you that, within <code>s</code>,
-         * the <code>count</code> characters beginning at <code>start</code>
-         * are about to be replaced by new text with length <code>after</code>.
-         * It is an error to attempt to make changes to <code>s</code> from
-         * this callback.
-         *
-         * @param s
-         * @param start
-         * @param count
-         * @param after
-         */
         @Override
         public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-            oldText = s.toString();
-            Log.d("Cell Edit", "Changing from " + oldText);
+            // do nothing
         }
 
-        /**
-         * This method is called to notify you that, within <code>s</code>,
-         * the <code>count</code> characters beginning at <code>start</code>
-         * have just replaced old text that had length <code>before</code>.
-         * It is an error to attempt to make changes to <code>s</code> from
-         * this callback.
-         *
-         * @param s
-         * @param start
-         * @param before
-         * @param count
-         */
         @Override
         public void onTextChanged(CharSequence s, int start, int before, int count) {
-            newText = s.toString();
-            Log.d("Cell Edit", "Changing to " + newText);
+            // do nothing
         }
 
-        /**
-         * This method is called to notify you that, somewhere within
-         * <code>s</code>, the text has been changed.
-         * It is legitimate to make further changes to <code>s</code> from
-         * this callback, but be careful not to get yourself into an infinite
-         * loop, because any changes you make will cause this method to be
-         * called again recursively.
-         * (You are not told where the change took place because other
-         * afterTextChanged() methods may already have made other changes
-         * and invalidated the offsets.  But if you need to know here,
-         * you can use {@link Spannable#setSpan} in {@link #onTextChanged}
-         * to mark your place and then look up from here where the span
-         * ended up.
-         *
-         * @param s
-         */
         @Override
         public void afterTextChanged(Editable s) {
-            if(newText.equals(oldText)) {
-              return;
-            } else if(newText.length() > 1) {
+            if(s.toString().equals("0")) {
                 s.clear();
-                s.append(oldText);
-                Log.d("Cell Edit", "Too long, reverting");
-            } else {
-                Log.d("Cell Edit", "New length fine, keeping");
+                s.append(" ");
             }
+            clearFocus();
+            requestFocus();
         }
     }
 
+    private class FocusListener implements OnFocusChangeListener {
 
+        ColorStateList oldColors = getTextColors();
+
+        @Override
+        public void onFocusChange(View v, boolean hasFocus) {
+            if(hasFocus) {
+                setTextColor(getResources().getColor(R.color.colorAccent));
+            } else {
+                setTextColor(oldColors);
+            }
+        }
+    }
 }
